0ed68ce7adca372dfd47949ca724f74b9c74a88e
likhithar2507 <73028560+likhithar2507@users.noreply.github.com>
2022-03-02 22:30:04 +0530

    program4

---
+++ program4.py
@@ -0,0 +1,3 @@
+# WAP to accept a string and count the frequency of each consonent.
+User_input = input("Enter a word: ")
+print(User_input.count('a')+User_input.count('e')+User_input.count('i')+User_input.count('o')+User_input.count('u'))
